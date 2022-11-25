64c7873bdefba9e0a8fae5aa7ebe6dc6ee54fc05
Erik Sylvan <erik.sylvan@gmail.com>
2022-11-26 00:29:27 +0100

    pytest decorator

--- test/test_01.py
+++ test/test_01.py
@@ -11,6 +11,15 @@ def test_depthCounterSlidingWindow_1():
 def test_depthCounterSlidingWindow_2():
     assert day_01.depthIncreaseCounterSlidingWindow([1,1,1,2,1,1,1]) == 1
 
+@pytest.mark.skip(reason="Skipped for no reason")
+def test_skippedTest():
+    assert False
 
 # Run tests from terminal:
 # $ pytest test/test_day_01.py 
+
+
+
+
+# read more
+# https://semaphoreci.com/community/tutorials/testing-python-applications-with-pytest
