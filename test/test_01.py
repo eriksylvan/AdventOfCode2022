2f04caa8dbda8ca6f9b2885dbb11bfc5fd3e2849
Erik Sylvan <erik.sylvan@gmail.com>
2022-11-29 17:18:45 +0100

    commit

--- test/test_01.py
+++ test/test_01.py
@@ -9,7 +9,7 @@ def test_depthCounterSlidingWindow_1():
     assert day_01.depthIncreaseCounterSlidingWindow([199,200,208,210,200,207,240,269,260,263]) == 5
 
 def test_depthCounterSlidingWindow_2():
-    assert day_01.depthIncreaseCounterSlidingWindow([1,1,1,2,1,1,1]) == 1
+    assert day_01.depthIncreaseCounterSlidingWindow([1,1,1,2,1,1,1]) == 109
 
 @pytest.mark.skip(reason="Skipped for no reason")
 def test_skippedTest():
