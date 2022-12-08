cd68ef8a888a4bea1ed5d254b6dc1b0b3df997bf
Erik Sylvan <erik.sylvan@gmail.com>
2022-12-08 15:07:30 +0100

    VIP day8

--- day_08.py
+++ day_08.py
@@ -3,8 +3,8 @@
 # https://adventofcode.com/2022/day/8
 
 
-inputFile='input/08_input'
-# inputFile='input/08_test_input'
+# inputFile='input/08_input'
+inputFile='input/08_test_input'
 
 
 
@@ -15,8 +15,19 @@ with open(inputFile) as input:
         trees.append(row.strip())
     h=len(trees)
     v=len(trees[0])
+
 print(h,v)
 
+
+def isVisibleFromNorth(th,vp,hp):return False
+def isVisibleFromSoth(th,vp,hp):return False
+def isVisibleFromEast(th,vp,hp):
+    ret = True
+    for i in range(vp+1,v):
+        ret = ret and (int(trees[hp][i]) < th)
+    return ret
+
+def isVisibleFromWest(th,vp,hp):return False
 
 # loop through all trees
 for vp in range(1,v-1):
@@ -25,18 +36,11 @@ for vp in range(1,v-1):
         pass
         th=int(trees[vp][hp])
         print(th,end='')
-        bo = isVisibleFromNorth(th,vp,hp) and isVisibleFromSoth(th,vp,hp) and isVisibleFromEast(th,vp,hp) and isVisibleFromWest(th,vp,hp)
+        # bo = isVisibleFromNorth(th,vp,hp) and isVisibleFromSoth(th,vp,hp) and isVisibleFromEast(th,vp,hp) and isVisibleFromWest(th,vp,hp)
+        isVisibleFromEast(th,vp,hp) 
     print()
 
 
-def isVisibleFromNorth(th,vp,hp):return False
-def isVisibleFromSoth(th,vp,hp):return False
-def isVisibleFromEast(th,vp,hp):
-    ret = True
-    for i in range(vp+1:v):
-        if trees[hp][i]
-    return False
 
-def isVisibleFromWest(h,v,h):return False
 
 
